c41f002658033279ae7c3e4f63da83a4fc76b89e
pmbadger <pm@steepshot.io>
2020-03-15 21:55:53 +0300

    Add required server functionality

---
+++ task5/src/main/java/com/bsu/CheckServlet.java
@@ -0,0 +1,14 @@
+package com.bsu;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CheckServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("application/json");
+        response.getOutputStream().println("{\"success\": true}");
+    }
+}
---
+++ task5/src/main/java/com/bsu/StatusServlet.java
@@ -0,0 +1,13 @@
+package com.bsu;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class StatusServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.getOutputStream().println("<html><h2 style='color:red'>Application Is Running</h2></html>");
+    }
+}

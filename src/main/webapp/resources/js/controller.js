5a8dff9f0c49f31defc8adccc80958dcfc3b0a4a
asenyarb <dimoniss00@gmail.com>
2020-04-28 22:08:01 +0300

    Fix likes are not save in collection bug

--- src/main/webapp/resources/js/controller.js
+++ src/main/webapp/resources/js/controller.js
@@ -2,10 +2,11 @@ class Controller{
     set_like(post, username){
         const userIndexInLikes = post.postLikes.indexOf(username);
         if (userIndexInLikes !== -1){
-            post.postLikes.splice(userIndexInLikes, 1);
+            post.postLikes = post.postLikes.splice(userIndexInLikes, 1);
         } else {
             post.postLikes.push(username);
         }
+        window.posts.edit(post.id, {postLikes: post.postLikes});
     }
 
     delete(id){
